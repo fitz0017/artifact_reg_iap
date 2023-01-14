9a818c5d2a12078cb3c461a791c2e98b12e923cb
Kevin <fitz0017@users.noreply.github.com>
2023-01-13 21:22:40 -0500

    printing token function

--- modules/cloud_function_gen2/main.tf
+++ modules/cloud_function_gen2/main.tf
@@ -1,20 +1,26 @@
-resource "google_cloudfunctions_function" "function" {
-  name        = "artifact-registry-print-token"
-  description = "Print OAuth token for client download."
-  runtime     = "python37"
+resource "google_cloudfunctions2_function" "function" {
+  name        = "function-v2"
+  location    = "us-central1"
+  description = "a new function"
 
-  available_memory_mb          = 128
-  source_archive_bucket        = google_storage_bucket.bucket.name
-  source_archive_object        = google_storage_bucket_object.archive.name
-  trigger_http                 = true
-  https_trigger_security_level = "SECURE_ALWAYS"
-  timeout                      = 60
-  entry_point                  = "/print_token"
-  labels = {
-    env = "prod-images"
+  build_config {
+    runtime     = "nodejs16"
+    entry_point = "helloHttp" # Set the entry point 
+    source {
+      storage_source {
+        bucket = google_storage_bucket.bucket.name
+        object = google_storage_bucket_object.object.name
+      }
+    }
   }
 
-  environment_variables = {
-    MY_ENV_VAR = "my-env-var-value"
+  service_config {
+    max_instance_count = 1
+    available_memory   = "256M"
+    timeout_seconds    = 60
   }
+}
+
+output "function_uri" {
+  value = google_cloudfunctions2_function.function.service_config[0].uri
 }
--- src/cloudrun/app.py
+++ src/cloudrun/app.py
@@ -1,4 +1,8 @@
 from flask import Flask, request, session
+import google.auth
+import subprocess
+# TODO - understand google auth library for python to print bearer token impresonating
+# the artifact registry account already created.
 
 app = Flask(__name__)
 
@@ -10,12 +14,18 @@ def index():
     page = f.read()
     f.close()
     # page += f"Welcome, {}"
+    # creds = google.auth.default()
+    # print(repr(creds))
+    creds = subprocess.check_output(
+        "gcloud auth print-access-token", shell=True)
+    print(creds.strip())
+    page = page.replace("{{ creds }}", repr(creds))
     return page
 
 # Login processing will need to integrate IAP
 
 
-@app.route('/login', methods=["GET", "POST"])
+@ app.route('/login', methods=["GET", "POST"])
 def login():
     if request.method == "POST":
         username = request.form["name"]
@@ -26,7 +36,7 @@ def login():
         # Secondary page after login to allow creation of 60min token for auth with AR
 
 
-@app.route('/request_token')
+@ app.route('/request_token')
 def request_token():
     pass
 
